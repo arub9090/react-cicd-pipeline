9490c6c6ed9c1954f01fc9ee640f9a9daab3f9c2
Arif Rubayet <aruba055@uottawa.ca>
2022-08-03 11:39:39 -0400

    This is a 2nd day Test only

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <h1> Hello World </h1>
         <h2>This is Arif Rubayet</h2>
         <h2>Done Creating CICD now. Alhumdulillah!! </h2>
+        <h4>This is Day 2- just testing if the service is running fine or not</h4>
         </p>
         <a
           className="App-link"
